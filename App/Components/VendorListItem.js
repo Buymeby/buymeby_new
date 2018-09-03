dfb396db2b03c76a930602929fdbab302243720d
gnaratil2017 <greg.naratil@gmail.com>
2018-09-03 16:40:20 -0400

    abstracted similar code out for vendor open/closed/open soon/closing soon status

--- App/Components/VendorListItem.js
+++ App/Components/VendorListItem.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { NavigationActions } from 'react-navigation'
-import timeToHumanReadable from '../Lib/OperationalHoursHelper'
+import {
+  timeToHumanReadable,
+  getCurrentHours,
+  getStatusColor,
+  getStatusText
+} from '../Lib/OperationalHoursHelper'
 import {
   TouchableOpacity,
   Image,
@@ -28,92 +33,6 @@ class VendorListItem extends React.Component {
     super(props)
   }
 
-  days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-
-  status = {
-    open_soon: {
-      color: 'green',
-      display_text: 'Opening Soon'
-    },
-    close_soon: {
-      color: 'orange',
-      display_text: 'Closing Soon'
-    },
-    open: {
-      color: 'green',
-      display_text: 'Open'
-    },
-    closed: {
-      color: 'red',
-      display_text: 'Closed'
-    }
-  }
-
-  getDayHours(hours) {
-    const current_date = new Date()
-    const day_index = current_date.getDay()
-    return hours.find(day_hours => day_hours.day === this.days[day_index])
-  }
-
-  getVendorStatus(hours) {
-    const current_date = new Date()
-    const current_hour = current_date.getHours()
-    const current_minutes = current_date.getMinutes()
-    let day_hours = this.getDayHours(hours)
-    if (day_hours) {
-      const open_hour = new Date(day_hours.open_time).getHours()
-      const open_minutes = new Date(day_hours.open_time).getMinutes()
-      const close_hour = new Date(day_hours.close_time).getHours()
-      const close_minutes = new Date(day_hours.close_time).getMinutes()
-      if ((!(current_hour === close_hour && current_minutes < close_minutes)) &&
-         ((current_hour === open_hour && current_minutes < open_minutes) ||
-         (current_hour === open_hour-1 && current_minutes >= open_minutes) ||
-         (open_hour === 0 && current_hour === 23 && current_minutes >= open_minutes)))
-      {
-        return 'open_soon'
-      } else if ((!(current_hour === open_hour && current_minutes < open_minutes)) &&
-                ((current_hour === close_hour && current_minutes < close_minutes) ||
-                (current_hour === close_hour-1 && current_minutes >= close_minutes) ||
-                (close_hour === 0 && current_hour === 23 && current_minutes >= close_minutes)))
-
-      {
-        return 'close_soon'
-      } else if ((open_hour > close_hour && (current_hour < close_hour || current_hour > open_hour)) ||
-                (open_hour > close_hour && current_hour === open_hour && current_minutes >= open_minutes && current_hour > close_hour) ||
-                (close_hour > open_hour && current_hour === open_hour && current_minutes >= open_minutes && current_hour < close_hour) ||
-                (close_hour > open_hour && current_hour > open_hour && current_hour < close_hour))
-      {
-        return 'open'
-      } else {
-        return 'closed'
-      }
-    } else {
-      return 'closed'
-    }
-  }
-
-  displayCurrentHours(hours) {
-    if (hours) {
-      let day_hours = this.getDayHours(hours)
-      if (day_hours && day_hours.open_time && day_hours.close_time) {
-        return (
-          <Caption>
-            {': ' + timeToHumanReadable(day_hours.open_time) + ' - ' + timeToHumanReadable(day_hours.close_time)}
-          </Caption>
-        )
-      }
-    }
-  }
-
-  printStatus(hours) {
-    if (hours) {
-      vendor_status = this.status[this.getVendorStatus(hours)]
-      return <Caption style={{color: vendor_status.color}}>{vendor_status.display_text}</Caption>
-    } else {
-      return <Caption style={{color: 'red'}}>Closed</Caption>
-    }
-  }
-
   render () {
     const { vendor } = this.props
     const { key } = this.props
@@ -128,8 +47,8 @@ class VendorListItem extends React.Component {
           <View styleName="vertical stretch space-between">
             <Subtitle>{vendor.name}</Subtitle>
             <View styleName="horizontal">
-              {this.printStatus(vendor.hours)}
-              {this.displayCurrentHours(vendor.hours)}
+              <Caption style={{color: getStatusColor(vendor.hours)}}>{getStatusText(vendor.hours)}</Caption>
+              <Caption>{getCurrentHours(vendor.hours)}</Caption>
             </View>
             <View styleName="horizontal">
               <Caption>{shorten_description(vendor.description)}</Caption>
--- App/Containers/VendorDetailsScreen.js
+++ App/Containers/VendorDetailsScreen.js
@@ -4,6 +4,12 @@ import { NavigationActions } from 'react-navigation'
 import VendorActions from '../Redux/VendorRedux'
 import getDirections from 'react-native-google-maps-directions'
 import {
+  timeToHumanReadable,
+  getCurrentHours,
+  getStatusColor,
+  getStatusText
+} from '../Lib/OperationalHoursHelper'
+import {
   ScrollView,
   TouchableOpacity,
   Image,
@@ -26,7 +32,6 @@ import {
 } from '@shoutem/ui'
 
 import LoadingSpinner from '../Components/LoadingSpinner'
-import timeToHumanReadable from '../Lib/OperationalHoursHelper'
 import Chevron from '../Components/Chevron'
 
 import styles from './Styles/ItemDetailsScreenStyles'
@@ -37,90 +42,6 @@ class VendorDetailsScreen extends Component {
   }
 
   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-  status = {
-    open_soon: {
-      color: 'green',
-      display_text: 'Opening Soon'
-    },
-    close_soon: {
-      color: 'orange',
-      display_text: 'Closing Soon'
-    },
-    open: {
-      color: 'green',
-      display_text: 'Open'
-    },
-    closed: {
-      color: 'red',
-      display_text: 'Closed'
-    }
-  }
-
-  getDayHours(hours) {
-    const current_date = new Date()
-    const day_index = current_date.getDay()
-    return hours.find(day_hours => day_hours.day === this.days[day_index])
-  }
-
-  getVendorStatus(hours) {
-    const current_date = new Date()
-    const current_hour = current_date.getHours()
-    const current_minutes = current_date.getMinutes()
-    let day_hours = this.getDayHours(hours)
-    if (day_hours) {
-      const open_hour = new Date(day_hours.open_time).getHours()
-      const open_minutes = new Date(day_hours.open_time).getMinutes()
-      const close_hour = new Date(day_hours.close_time).getHours()
-      const close_minutes = new Date(day_hours.close_time).getMinutes()
-      if ((!(current_hour === close_hour && current_minutes < close_minutes)) &&
-         ((current_hour === open_hour && current_minutes < open_minutes) ||
-         (current_hour === open_hour-1 && current_minutes >= open_minutes) ||
-         (open_hour === 0 && current_hour === 23 && current_minutes >= open_minutes)))
-      {
-        return 'open_soon'
-      } else if ((!(current_hour === open_hour && current_minutes < open_minutes)) &&
-                ((current_hour === close_hour && current_minutes < close_minutes) ||
-                (current_hour === close_hour-1 && current_minutes >= close_minutes) ||
-                (close_hour === 0 && current_hour === 23 && current_minutes >= close_minutes)))
-
-      {
-        return 'close_soon'
-      } else if ((open_hour > close_hour && (current_hour < close_hour || current_hour > open_hour)) ||
-                (open_hour > close_hour && current_hour === open_hour && current_minutes >= open_minutes && current_hour > close_hour) ||
-                (close_hour > open_hour && current_hour === open_hour && current_minutes >= open_minutes && current_hour < close_hour) ||
-                (close_hour > open_hour && current_hour > open_hour && current_hour < close_hour))
-      {
-        return 'open'
-      } else {
-        return 'closed'
-      }
-    } else {
-      return 'closed'
-    }
-  }
-
-  displayCurrentHours(hours) {
-    if (hours) {
-      let day_hours = this.getDayHours(hours)
-      if (day_hours && day_hours.open_time && day_hours.close_time) {
-        return (
-          <Text>
-            {': ' + timeToHumanReadable(day_hours.open_time) + ' - ' + timeToHumanReadable(day_hours.close_time)}
-          </Text>
-        )
-      }
-    }
-  }
-
-  printStatus(hours) {
-    if (hours) {
-      vendor_status = this.status[this.getVendorStatus(hours)]
-      return <Text style={{color: vendor_status.color}}>{vendor_status.display_text}</Text>
-    } else {
-      return <Text style={{color: 'red'}}>Closed</Text>
-    }
-  }
 
   static navigationOptions = ({ navigation, navigationOptions }) => {
     return {
@@ -191,8 +112,8 @@ class VendorDetailsScreen extends Component {
             </Tile>
             <Row>
               <Subtitle style={{textAlign: 'center'}}>
-                {this.printStatus(vendor.hours)}
-                {this.displayCurrentHours(vendor.hours)}
+                <Text style={{color: getStatusColor(vendor.hours)}}>{getStatusText(vendor.hours)}</Text>
+                <Text>{getCurrentHours(vendor.hours)}</Text>
               </Subtitle>
             </Row>
             <Row>
