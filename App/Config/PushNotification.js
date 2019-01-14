d7faa717b12121a6f9f8d27e72ea5292951641fb
H-KE <michael.kehan@gmail.com>
2019-01-13 19:39:15 -0500

    android push

--- App/Config/PushNotification.js
+++ App/Config/PushNotification.js
@@ -2,17 +2,22 @@ import PushNotification from 'react-native-push-notification'
 import { PushNotificationIOS, Platform } from 'react-native'
 
 const configure = (api) => {
+  // console.tron.log("Push notification configured")
   PushNotification.configure({
+    onError: function(e) {
+      // console.tron.log(e)
+    },
     onRegister: function(token) {
       api.registerDevice(token)
     },
 
     onNotification: function(notification) {
-     console.tron.log(notification)
+     // console.tron.log(notification)
 
      if (Platform.OS === 'ios') notification.finish(PushNotificationIOS.FetchResult.NoData)
     },
 
+    senderID: '819870663726',
     permissions: {
      alert: true,
      badge: true,
@@ -22,6 +27,7 @@ const configure = (api) => {
     popInitialNotification: true,
     requestPermissions: true
   })
+  PushNotification.requestPermissions()
 }
 
 export {
